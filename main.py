7fb0c9657f0804d633111c1612d98a40a443e03b
andrewswanback <atswanback@gmail.com>
2021-02-19 03:49:01 -0500

    files

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import time
 import re
-import requests  # to get image from the web
-import shutil  # to save it locally
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from editing import *
+from misc import *
+from youtube import *
+from pexels import *
+
+##################
+name = 'andrew'
+#name = 'caleb'
+##################
+
+path_dict = {
+    'andrew': '/Users/andrewswanback/Documents/selenium_data',
+    'caleb': '/Users/calebstevens/Downloads'
+}
+path = path_dict[name]  #anytime you need to use your full filepath, use 'path'
+
+def set_dir(name):
+    full_path = '{}/{}'.format(path,name)
+    if not os.path.exists(full_path):
+        os.mkdir(full_path)
+    return full_path
+
 
 if __name__ == "__main__":
-    web = webdriver.Chrome('chrome/chromedriver')
-    web.quit()
+    #dir_1 = set_dir('photos')
+    #get_pexel_photos('wave',dir_1)
+
+    #dir_2 = set_dir('videos')
+    #get_pexel_videos('wave',dir_2)
+
+    dir_3 = set_dir('audio')
+    get_yt_audios(dir_3)
+
+
+
